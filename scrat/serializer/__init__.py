7210f758404d6b593db5b9aa94bb3ba2a54fd230
javiber <javier.berneche@gmail.com>
2023-07-31 23:15:22 -0300

    New serializers and improved existing ones

--- scrat/serializer/__init__.py
+++ scrat/serializer/__init__.py
@@ -1,8 +1,15 @@
-from .base import Serializer  # NoQA
+from .base import Serializer  # noqa: F401
+from .dill import DillSerializer  # noqa: F401
+from .json import JsonSerializer
+from .numpy import NumpySerializer
 from .pandas import PandasSerializer
 from .pickle import PickleSerializer
 
-DEFAULT_SERIALIZERS = {"DataFrame": PandasSerializer()}
+DEFAULT_SERIALIZERS = {
+    "DataFrame": PandasSerializer,
+    "Series": PandasSerializer,
+    "ndarray": NumpySerializer,
+}
 
 
 def get_default_serializer(func):
@@ -11,7 +18,7 @@ def get_default_serializer(func):
     sign = inspect.signature(func)
     if hasattr(sign, "return_annotations"):
         return DEFAULT_SERIALIZERS.get(
-            sign.return_annotation.__name__, PickleSerializer()
-        )
+            sign.return_annotation.__name__, PickleSerializer
+        )()
 
     return PickleSerializer()
--- scrat/serializer/dill.py
+++ scrat/serializer/dill.py
@@ -1,21 +1,27 @@
-import pickle
 import typing as T
 from pathlib import Path
 
 
-class PickleSerializer:
+class DillSerializer:
     def __init__(
         self,
         dump_kwargs: T.Optional[T.Dict[str, T.Any]] = None,
         load_kwargs: T.Optional[T.Dict[str, T.Any]] = None,
     ) -> None:
+        # test that dill is installed
+        import dill  # noqa: F401
+
         self.dump_kwargs = dump_kwargs if dump_kwargs is not None else {}
         self.load_kwargs = load_kwargs if load_kwargs is not None else {}
 
     def dump(self, obj: T.Any, path: Path):
+        import dill
+
         with open(path, "wb") as f:
-            pickle.dump(obj, f, **self.dump_kwargs)
+            dill.dump(obj, f, **self.dump_kwargs)
 
     def load(self, path: Path) -> T.Any:
+        import dill
+
         with open(path, "rb") as f:
-            return pickle.load(f, **self.load_kwargs)
+            return dill.load(f, **self.load_kwargs)
